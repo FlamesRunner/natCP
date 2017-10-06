83d3bcd1d307db1aaf7f7b31170096af3e77c9f8
Andrew H <FlamesRunner@users.noreply.github.com>
2017-10-06 12:07:26 -0400

    Create createFirstUser.php

---
+++ web/createFirstUser.php
@@ -0,0 +1,29 @@
+<?php
+require 'manager/db.php';
+
+function generatePassword($length = 8) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    $count = mb_strlen($chars);
+
+    for ($i = 0, $result = ''; $i < $length; $i++) {
+        $index = rand(0, $count - 1);
+        $result .= mb_substr($chars, $index, 1);
+    }
+
+    return $result;
+}
+
+$sql = 'INSERT INTO users (user_name, user_password_hash, user_email) VALUES (:username, :password, :email)';
+$tmpPass = generatePassword(16);
+$adduser = $dbh->prepare($sql);
+$adduser->bindParam(':username', 'admin');
+$adduser->bindParam(':password', password_hash($tmpPass, PASSWORD_DEFAULT));
+$adduser->bindParam(':email', 'tmpemail@tmpemail.com');
+$adduser->execute();
+
+echo "\n";
+echo "A user was created for you.\n"
+echo "Username: admin \n";
+echo "Password: " . $tmpPass . "\n";
+echo "Copy this down, and delete this file after you're done.";
+?>
