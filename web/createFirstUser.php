03a887eeca83ce327f5bc5d1750baac528c61cd4
sayghteight <duelista16@gmail.com>
2017-10-08 02:21:44 +0000

    Fix createFirstUser

--- web/createFirstUser.php
+++ web/createFirstUser.php
@@ -17,15 +17,17 @@ function generatePassword($length = 8) {
 }
 
 $sql = 'INSERT INTO users (user_name, user_password_hash, user_email) VALUES (:username, :password, :email)';
-$tmpPass = generatePassword(16);
+$tmpPass = generatePassword(8);
 $adduser = $dbh->prepare($sql);
-$adduser->bindParam(':username', 'admin');
+$user = 'admin';
+$email = 'tmpemail@tmpemail.com';
+$adduser->bindParam(':username', $user);
 $adduser->bindParam(':password', password_hash($tmpPass, PASSWORD_DEFAULT));
-$adduser->bindParam(':email', 'tmpemail@tmpemail.com');
+$adduser->bindParam(':email', $email);
 $adduser->execute();
 
 echo "\n";
-echo "A user was created for you.\n"
+echo "A user was created for you.\n";
 echo "Username: admin \n";
 echo "Password: " . $tmpPass . "\n";
 echo "Copy this down, and delete this file after you're done.";
